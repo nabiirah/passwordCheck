29a2a6de769c4f8f46ea77d96bbb403f7dc60a98
nabiirah <nabiirahkaseruuzi@gmail.com>
2020-01-02 12:09:12 +0000

    Edits to add getpass and user instructions for input

--- passwordCheck.py
+++ passwordCheck.py
@@ -1,26 +1,40 @@
 #!/usr/bin/python3.6
-##
 import re   #import regular expressions
+import getpass
 
-password=input("Enter password: ")
-x = True    #x is just a random variable
-while x:    #as long as x is True, then run the below loop
-    if (len(password)<6 or len(password)>12):
-        break
-    elif not re.search("[a-z]",password):
-        break
-    elif not re.search ("[A-Z]",password):
-        break
-    elif not re.search ("[0-9]",password):
-        break
-    elif not re.search ("[$#@]",password):
-        break
-    elif re.search("\s",password):  #if password contains white space  
-        break
-    else:
-        print("Your password has been set!")
-        x = False
-        break   #exit the while loop
+print (chr(27) + "[2J")
 
-if x:
-    print("Invalid Password")   #will exit program after printing this
+print ("\n\nCreate a new password that is between 6-12 characters long. A strong password contains at least one lowercase and uppercase letter, at least one digit and at least one special character.")
+def main():
+    password=getpass.getpass("\n\nEnter password: ")
+    x = True    # x is just a random variable set to True
+    while x:    # as long as x is True, then run the below loop
+        if (len(password)< 6):  #if password less then 6 characters long then exit loop
+            print("\nYour password is too short")
+            break
+        elif (len(password)> 12): #if password is more than 12 characters long then exit loop
+            print("\nYour password is too long")
+            break
+        elif not re.search("[a-z]",password): #if password doesn't have one lowercase letter then exit loop
+            print("\nYour password should contain at least one lowercase letter")
+            break
+        elif not re.search ("[A-Z]",password): #if password doesn't have at least one uppercase letter then exit loop
+            print("\nYour password should contain at least one uppercase letter")
+            break
+        elif not re.search ("[0-9]",password):  #if password doen't have at least one digit then exit loop
+            print("\nYour password should contain at least one digit")
+            break
+        elif not re.search ("[$#@&!*%?]",password):  #if password doen't have a special character then exit loop
+            print("\nYour password should contain at least one special character (i.e. $#@&!*%?)")
+            break
+        elif re.search("\s",password):  #if password has any white space
+            print("\nYour password shouldn't contain any white spaces")
+            break
+        else:
+            print("Your password has been set!")
+            x=False  #valid password has been created. Set x to False and exit loop
+            break
+    if x: #this is outside while loop
+        print (" ")  #this print will be executed at each invalid input alongside the above individual messages that will be printed on exiting the loop
+        main()
+main()
